059ed3600ea2074921239c0b2261188c0fe4bcd9
sundayScoop <killer.of.time@protonmail.com>
2025-11-12 13:57:13 +1000

    no need for tide-js anymore

--- packages/tidecloak-js/src/index.js
+++ packages/tidecloak-js/src/index.js
@@ -1,8 +1,4 @@
-// @ts-check
-
-import { ModelRegistry } from '../modules/tide-js/Models/ModelRegistry.js';
-
 export { default as IAMService } from './IAMService.js';
-export { default as TideCloak } from '../lib/tidecloak.js';
-export { bytesToBase64, base64ToBytes } from '../modules/tide-js/Cryptide/Serialization.js';
-export { RequestEnclave, ApprovalEnclave } from '../lib/tidecloak.js';
+export { default as TideCloak } from "../lib/tidecloak.js";
+export { RequestEnclave, ApprovalEnclave } from "../lib/tidecloak.js";
+export { TideMemory } from "heimdall-tide";
